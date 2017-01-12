e1fed97cdb997b95fa3397d370e38397e19f757f
Stepan Orlov <majorsteve@mail.ru>
2017-01-12 22:40:54 +0300

    Refactoring client code, take 2.

--- public/javascripts/multiplot.js
+++ public/javascripts/multiplot.js
@@ -2,9 +2,111 @@ var multiplot = (function() {
     function done(cb) { return function(data) { cb(null, data) } }
     function fail(cb) { return function(xhr) { cb(xhr) } }
 
+    function computeValueColumnForCurve(processingInfo, curveData, valueName, knownColumns) {
+        var d = curveData.data
+        if (d.length === 0)
+            return  // Empty dataset
+        var n = d.length
+        knownColumns = knownColumns || {}
+        if (_.isEmpty(knownColumns)) {
+            // Determine which columns are already known
+            knownColumns = {}
+            for (var columnName in d[0])
+                knownColumns[columnName] = 1
+        }
+
+        function computeExtent() {
+            if (!curveData.extent[valueName])
+                curveData.extent[valueName] = d3.extent(d, function(d) {return d[valueName]})
+        }
+
+        if (valueName in knownColumns)
+            return computeExtent() // Value is known
+
+        var recipe = processingInfo.computedValues[valueName]
+        if (!recipe)
+            throw new Error('No recipe for computing value ' + valueName)
+        var dependencies = recipe.dependencies || []
+        dependencies.forEach(function(dependencyName) {
+            if (!(dependencyName in knownColumns))
+                computeValueColumnForCurve(processingInfo, curveData, dependencyName, knownColumns)
+        })
+        var op = recipe.operation
+        var i
+        switch(op.type) {
+        case 'derivative':
+            if (n < 2)
+                d[0][valueName] = 0  // Unable to really compute derivative
+            else {
+                for (i=1; i<n; ++i)
+                    d[i-1][valueName] = (d[i][op.func] - d[i-1][op.func]) / (d[i][op.arg] - d[i-1][op.arg])
+                d[n-1][valueName] = d[n-2][valueName] // Pad the trailing element with the last value of the derivative
+            }
+            break
+        case 'formula':
+            var funcCtorArgs = [].concat(dependencies)
+            funcCtorArgs.push('return ' + op.formula)
+            var formula = Function.apply(Function, funcCtorArgs)
+            for (i=0; i<n; ++i) {
+                var funcArgs = []
+                dependencies.forEach(function(name) {
+                    funcArgs.push(d[i][name])
+                })
+                d[i][valueName] = formula.apply(null, funcArgs)
+            }
+            break
+        default:
+            throw new Error('Unsupported operation type "' + op.type + '" in recipe for value ' + valueName)
+        }
+        computeExtent()
+        knownColumns[valueName] = 1
+    }
+
     function computeDiagramData(multiplot, options, categoryInfo, cb)
     {
-        cb('TODO')
+        var cacheItem = multiplot.cache[options.dir]
+        var allCurveData = cacheItem.curveData
+        async.eachLimit(
+            categoryInfo,
+            10,
+            function(item, cb) {
+                async.waterfall(
+                    [
+                        function(cb) {
+                            var curveData = allCurveData[item.name]
+                            if (curveData)
+                                return done(cb)(curveData)
+                            $.get('/multiplot-file', { curdir: options.dir, name: item.name })
+                                .done(function(data) {
+                                    curveData = allCurveData[item.name] = {
+                                        data: d3.tsvParse(data, function(d) {
+                                            for(var col in d)
+                                                d[col] = +d[col]
+                                            return d
+                                        }),
+                                        extent: {}
+                                    }
+                                    done(cb)(curveData)
+                                })
+                                .fail(fail(cb))
+                            },
+                        function(curveData, cb) {
+                            var curveInfo = cacheItem.dirInfo.processing.curves[options.curveIndex]
+                            var knownColumns = {}
+                            var processingInfo = multiplot.cache[options.dir].dirInfo.processing
+                            computeValueColumnForCurve(processingInfo, curveData, curveInfo.x.value, knownColumns)
+                            computeValueColumnForCurve(processingInfo, curveData, curveInfo.y.value, knownColumns)
+                            done(cb)
+                        }
+                    ],
+                    function(err) {
+                        (err? fail: done)(cb)()
+                    }
+                )
+            },
+            function(err) {
+                (err? fail: done)(cb)()
+            })
     }
 
     function Multiplot()
